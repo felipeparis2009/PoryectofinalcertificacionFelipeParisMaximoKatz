83acb9ab9041e830afc8a7d19d1718ab1a33c479
felipeparis2009 <167116322+felipeparis2009@users.noreply.github.com>
2024-10-21 15:56:04 -0300

    Aca te va

---
+++ proyectofinalcertificacion.py
@@ -0,0 +1,20 @@
+#Vamos a Jugar al piedra papel o tijera
+from random import randint
+elegi = ["piedra, papel o tijera"]
+bot = elegi[randint(0,2)]
+jugador = input("elgi alguna de estas opciones: ").lower()
+print ("bot elige alguna estas opciones: " + bot)
+if jugador == bot:
+    print("empate")
+elif jugador == "piedra" and bot == "papel":
+    print("el bot gano")
+elif jugador == "piedra" and bot == "tijera":
+    print("ganaste")
+elif jugador == "papel" and bot == "tijera":
+    print("el bot gano")
+elif jugador == "papel" and bot == "piedra":
+    print("ganaste")
+elif jugador == "tijera" and bot == "piedra":
+    print("el bot gano")
+elif jugador == "tijera" and bot == "papel":
+    print("ganaste")pier 
